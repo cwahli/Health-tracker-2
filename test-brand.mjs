d47047fa4f2cb8505762269ede23e79fe9bb1255
cwah123 <chiwah.liu@sage.com>
2026-08-27 21:23:50 +0100

    chore: remove unused puppeteer dependency and stray scratch scripts, point main.tsx to client-error

--- test-brand.mjs
+++
@@ -1,5 +0,0 @@
-import { matchBrandMenu } from './dist/server_brand_match.js';
-(async () => {
-  const result = await matchBrandMenu(null, "Boiled Rolled Oats with Water", "Boiled Rolled Oats with Water");
-  console.log(result);
-})();
